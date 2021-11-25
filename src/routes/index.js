f4bca3b7e48fe56e0223776e6a38861a31570757
YaelCS06 <jcoronados1800@alumno.ipn.mx>
2021-11-25 16:36:15 -0600

    ayudadiositox6

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {isLoggedIn, isNotLoggedIn} =require('../lib/auth');
+
 
 router.get('/',  async (req, res) => {
     res.render('index');
