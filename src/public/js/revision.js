a36dfa388d8159e42a62ca5bbdeec1efba8e43e9
YaelCS06 <jcoronados1800@alumno.ipn.mx>
2021-12-01 22:45:41 -0600

    graciasdiosunux8

--- src/public/js/revision.js
+++ src/public/js/revision.js
@@ -5,13 +5,13 @@ function verificarRespuesta(){
     var arreglo = [];
     var respuestas = ["a","a","c","c","c","b","c","b","b","c","a","b","b","c","c","b","b","a","c","d","c","a","a","a","a","d","d","d","a","a","a","c","b","c","c","a","c","b","c","b","a","b","c","c","d","c","b","c","c","b","a","a","b","c","b","a","d","a","d","c","b","d","a","c","d","b","c","d","b","a","a","a","b","a","b","c","c","c","a","b"];
 
-    for(var i = 0; i <= total; i ++){
+
+    for(var i = 1; i <= myForm.length; i ++){
         
-        console.log(respuestas[i]);
-        if(myForm[i].value ===respuestas[i-1]){
+        if(myForm[i-1].value ===respuestas[i-1]){
                 puntos ++;
             }
-            if(i==10||i==20||i==30||i==40||i==50||i==60||i==70||i==80){
+            if(i==10||i==20||i==30||i==40||i==50||i==60||i==70||i==79){
                 arreglo.push(puntos);
             }
     }
@@ -23,9 +23,17 @@ function verificarRespuesta(){
     var r6 = arreglo[5]-(r5+r4+r3+r2+r1);
     var r7 = arreglo[6]-(r6+r5+r4+r3+r2+r1);
     var r8 = arreglo[7]-(r7+r6+r5+r4+r3+r2+r1);
-  
-    swal("Tu resultado es : "+ puntos);
     
+    document.getElementById("cuadrito").value = puntos;
+    document.getElementById("matematicas").value = r1;
+    document.getElementById("espa").value = r2;
+    document.getElementById("biologia").value = r3;
+    document.getElementById("fisica").value = r4;
+    document.getElementById("quimica").value = r5;
+    document.getElementById("historia").value = r6;
+    document.getElementById("geografia").value = r7;
+    document.getElementById("formacion").value = r8;
+    document.rexam.submit();
 }
 function verificarRespuestaCu(){
     var total = 10;
