1288bd53525ca1ca937c80f13b8ed9ffee7c4c07
YaelCS06 <jcoronados1800@alumno.ipn.mx>
2021-12-02 15:15:49 -0600

    ayudafinaldiospofavox2

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
     database: {
         host:'nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         user: 'ijcjlt40n2ca26kb',
-        password: 'kdlxsqnnlfuknemc',
+        password: 'tlajvvd9q83hlbu2',
         database: 'ftlrtzz0bdo7ufhy'
     }
 };
